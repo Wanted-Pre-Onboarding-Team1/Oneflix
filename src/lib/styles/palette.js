b479afe85ee1cae5133718763579027641844a81
이지호 <cctv1998@naver.com>
2022-07-07 22:43:29 +0900

    feat : background 보다 밝은 컬러들 추가

--- src/lib/styles/palette.js
+++ src/lib/styles/palette.js
@@ -3,4 +3,6 @@ export const palette = {
   backgroundColor: '#191919',
   fontColor: '#E1E1E1',
   borderColor: '#4c4c4c',
+  backgroundLightColor: '#2d2d2d',
+  backgroundLighterColor: '#565656',
 };
