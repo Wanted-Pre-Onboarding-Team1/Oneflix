83b57ce04bafd9fadacf690ee9337bc1e1506e33
HyeonJu <hhjc017@gmail.com>
2022-07-07 19:33:29 +0900

    style : te텍스트 스타일 재사용 추가

--- src/lib/styles/palette.js
+++ src/lib/styles/palette.js
@@ -1,4 +1,7 @@
 export const palette = {
   mainColor: '#F33F3F',
   backgroundColor: '#E1E1E1',
+  btnTextColor: '#141414',
+  favoriteTextColor: '#9A9A9A',
+  sidebarBackgroundColor: '#000',
 };
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,7 +1,42 @@
+import styled, { css } from 'styled-components';
 import React from 'react';
 
 function LandingPage() {
-  return <div>LandingasdasPage</div>;
+  return (
+    <div>
+      <CardText color="btn" fontSize="16" fontWeight="700" lineHeight="19" />
+    </div>
+  );
 }
 
 export default LandingPage;
+
+const LandingPageColorPicker = (palette, color) => {
+  switch (color) {
+    case 'main':
+      return palette.mainColor;
+    case 'backgroundColor':
+      return palette.backgroundColor;
+    case 'btn':
+      return palette.btnTextColor;
+    case 'favorite':
+      return palette.favoriteTextColor;
+    case 'sidebar':
+      return palette.sidebarBackgroundColor;
+    default:
+      return '';
+  }
+};
+
+const LandingPageTextStyle = styled.css`
+  ${(color, fontSize, fontWeight, lineHeight) => css`
+    color: ${LandingPageColorPicker(color)};
+    font-size: ${fontSize}px;
+    font-weight: ${fontWeight};
+    line-height: ${lineHeight}px;
+  `}
+`;
+
+const CardText = styled.strong`
+  ${LandingPageTextStyle};
+`;
