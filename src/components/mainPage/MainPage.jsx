ee08360c152cb7ed7dcf0eca9fdfd066411094d0
HyeonJu <hhjc017@gmail.com>
2022-07-08 18:04:27 +0900

    #12 feature : mainPage infinite scroll 구현

--- src/components/mainPage/MainPage.jsx
+++ src/components/mainPage/MainPage.jsx
@@ -1,22 +1,37 @@
 import axios from 'axios';
 import MovieCard from 'components/movieCard/MovieCard';
+import useIntersectObserver from 'hooks/useIntersectObserver';
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
-const DOMAIN = 'http://localhost:8000/';
+const DOMAIN = 'http://localhost:8080/';
 
 const MainPage = function () {
   const [MovieList, setMovieList] = useState([]);
+  const [isInitialLoading, setInitialLoading] = useState(true);
+  const { isTargetVisible, observeTargetRef } = useIntersectObserver();
+
   useEffect(() => {
     const getMovieList = async () => {
       const response = await axios.get(`${DOMAIN}movies?_page=1`);
       const { data } = response;
-      console.log(data);
       setMovieList([...data]);
     };
 
     getMovieList();
   }, []);
+
+  useEffect(() => {
+    setInitialLoading(false);
+
+    const getMoreMovies = async () => {
+      const response = await axios.get(`${DOMAIN}movies?_page=2`);
+      const { data } = response;
+      setMovieList((prev) => [...prev, ...data]);
+    };
+
+    isTargetVisible && !isInitialLoading && getMoreMovies();
+  }, [isTargetVisible, isInitialLoading]);
 
   return (
     <MainPageLayout>
@@ -41,6 +56,7 @@ const MainPage = function () {
           ),
         )}
       </ul>
+      <div ref={observeTargetRef}>observe target</div>
     </MainPageLayout>
   );
 };
@@ -49,6 +65,7 @@ export default MainPage;
 
 export const MainPageLayout = styled.section`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   width: 100%;
   padding: 90px 30px;
