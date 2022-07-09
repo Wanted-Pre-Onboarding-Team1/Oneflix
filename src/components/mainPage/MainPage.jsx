49d132a8748203d33c492e62d611d01c14e2b19a
HyeonJu <hhjc017@gmail.com>
2022-07-09 13:27:46 +0900

    #12 feature : getCurrentPageNumber 로직 수정

--- src/components/mainPage/MainPage.jsx
+++ src/components/mainPage/MainPage.jsx
@@ -10,27 +10,30 @@ function MainPage() {
   const { isTargetVisible, observeTargetRef } = useIntersectObserver();
   const movieRequest = new HttpRequest();
 
-  const getCurrentPage = (list) => list.length * 0.1;
+  const getCurrentPageNumber = (list) => {
+    const pageNumber = list.length * 0.1;
+    return Number.isInteger(pageNumber) ? pageNumber : Math.ceil(pageNumber);
+  };
 
   useEffect(() => {
     const callback = (response) => setMovieList(response.data);
 
     movieRequest.getWithParams({
       url: 'movies',
-      config: { _page: getCurrentPage(movieList) },
+      config: { _page: getCurrentPageNumber(movieList) },
       callback,
     });
   }, []);
 
   useEffect(() => {
-    setInitialLoading(false);
+    getCurrentPageNumber(movieList) === 1 && setInitialLoading(false);
     const callback = ({ data }) => setMovieList((prev) => [...prev, ...data]);
 
     isTargetVisible &&
       !isInitialLoading &&
       movieRequest.getWithParams({
         url: 'movies',
-        config: { _page: getCurrentPage(movieList) + 1 },
+        config: { _page: getCurrentPageNumber(movieList) + 1 },
         callback,
       });
   }, [isTargetVisible, isInitialLoading]);
