97fe934cae904b49a9789c824b211fd0175ec31d
godcl1623 <delphi5281@gmail.com>
2022-07-09 18:14:15 +0900

    #25 fix : infinite scroll merge에 따른 충돌 사항 수정

--- src/lib/styles/palette.js
+++ src/lib/styles/palette.js
@@ -1,12 +1,13 @@
 export const palette = {
   mainColor: '#F33F3F',
   backgroundColor: '#191919',
-  backgroundColorLight: '#2d2d2d',
+  backgroundColorCard: '#FFFFFF',
+  backgroundColorLight: '#2D2D2D',
   backgroundColorLighter: '#565656',
   sideBackgroundColor: '#000000',
   sideTextColor: '#E1E1E1',
   sideTabColor: '#1F1F1F',
   highlightColor: '#BB65FF',
   fontColor: '#E1E1E1',
-  borderColor: '#4c4c4c',
+  borderColor: '#4C4C4C',
 };
