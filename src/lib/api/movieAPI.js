7d1bd3fcbc40b47389bfd88236b757cc7f8809ab
이지호 <cctv1998@naver.com>
2022-07-11 13:34:22 +0900

    feat : movieAPI 년도 검색 가능 기능

--- src/lib/api/movieAPI.js
+++ src/lib/api/movieAPI.js
@@ -1,9 +1,10 @@
 import { HttpRequest } from 'lib/api/httpRequest';
 
 const request = new HttpRequest();
-export const getMoviesAPI = async (title, page) => {
+export const getMoviesAPI = async (title, year, page) => {
   const response = await request.get(
-    `/movies?_limit=20&_page=${page}&q=${title}`,
+    `/movies?_limit=20&_page=${page}&year_like=${year}&q=${title}`,
   );
   return response.data;
 };
+// &year=${year}
